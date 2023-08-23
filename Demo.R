79ab7b836ef22cf13a2f852f7c421128688d03e4
Ayana Kono <ayana.kono2000.0329@gmail.com>
2023-08-23 10:05:05 +0900

    select countries

--- Demo.R
+++ Demo.R
@@ -1,3 +1,7 @@
 df <- readr::read_csv("owid-co2-data.csv")
 head(df)
+View(df)
 
+df |> 
+  dplyr::select(country, year, gdp, co2_per_capita, population) |> 
+  dplyr::filter(country == c("Canada", "France", "Germany", "Italy", "Japan", "United Kingdom", "United States")) |> 

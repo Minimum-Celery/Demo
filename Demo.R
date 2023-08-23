23d3c9c314ed11e27ea2c26acd1078f9d2f2b417
Ayana Kono <ayana.kono2000.0329@gmail.com>
2023-08-23 10:31:56 +0900

    plot

--- Demo.R
+++ Demo.R
@@ -6,8 +6,11 @@ df2 <- df |>
   dplyr::select(country, year, gdp, co2_per_capita, population) |> 
   dplyr::filter(country == c("Canada", "France", "Germany", "Italy", "Japan", "United Kingdom", "United States"),
                 !is.na(gdp)) |> 
-  dplyr::mutate(gdp_per_capita = gdp / population)
+  dplyr::mutate(gdp_per_capita = gdp / population) |> 
+  dplyr::mutate(gdp_per_capita_in_1000 = gdp_per_capita / 1000)
 
 df2 |> 
   ggplot2::ggplot() +
-  ggplot2::geom_line(ggplot2::aes(x = year))
+  ggplot2::geom_line(ggplot2::aes(x = gdp_per_capita_in_1000, y = co2_per_capita)) +
+  ggplot2::facet_wrap(~ country) +
+  ggplot2::labs(x = "gdp per capita(1000 USD)", y = "CO2 per capita")

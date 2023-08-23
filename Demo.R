ad25119a0534d02db17893735de58c064d9bf8d6
Chris Taki <jamubin@gmail.com>
2023-08-23 10:20:34 +0900

    Crete gdp_per_capita and create ggplot

--- Demo.R
+++ Demo.R
@@ -1,7 +1,13 @@
 df <- readr::read_csv("owid-co2-data.csv")
 head(df)
-View(df)
+View(df2)
 
-df |> 
+df2 <- df |> 
   dplyr::select(country, year, gdp, co2_per_capita, population) |> 
-  dplyr::filter(country == c("Canada", "France", "Germany", "Italy", "Japan", "United Kingdom", "United States")) |> 
+  dplyr::filter(country == c("Canada", "France", "Germany", "Italy", "Japan", "United Kingdom", "United States"),
+                !is.na(gdp)) |> 
+  dplyr::mutate(gdp_per_capita = gdp / population)
+
+df2 |> 
+  ggplot2::ggplot() +
+  ggplot2::geom_line(ggplot2::aes(x = year))
